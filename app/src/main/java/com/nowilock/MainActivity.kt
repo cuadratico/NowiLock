d1b775887cbb0dd0eac8cb1b1750ab1df42be9d9
Tirsi <tirsi333@gmail.com>
2025-08-08 20:17:37 +0200

    Without workers, only with foregroundService.

--- app/src/main/java/com/nowilock/MainActivity.kt
+++ app/src/main/java/com/nowilock/MainActivity.kt
@@ -1,24 +1,39 @@
 package com.nowilock
 
 import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.shapes.Shape
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import androidx.work.Constraints
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.Worker
+import com.google.android.material.imageview.ShapeableImageView
 import com.nowilock.db.Companion.logs_list
 import com.nowilock.recy.adapter_logs
 import kotlinx.coroutines.CoroutineStart
@@ -30,6 +45,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.security.KeyStore
 import java.util.Base64
+import java.util.jar.Manifest
 import javax.crypto.Cipher
 import javax.crypto.spec.GCMParameterSpec
 
@@ -47,6 +63,9 @@ class MainActivity : AppCompatActivity() {
         val alias = intent.extras?.getString("ali").orEmpty()
         val recy = findViewById<RecyclerView>(R.id.recy)
         val information = findViewById<TextView>(R.id.info)
+        val icon_service = findViewById<ShapeableImageView>(R.id.icon_service)
+        val service_status = findViewById<ConstraintLayout>(R.id.service_status)
+        val activate_info = findViewById<ShapeableImageView>(R.id.activate_info)
         information.visibility = View.INVISIBLE
         val db = db(this)
 
@@ -54,8 +73,21 @@ class MainActivity : AppCompatActivity() {
         recy.adapter = adapter
         recy.layoutManager = LinearLayoutManager(this)
 
-        val request = OneTimeWorkRequestBuilder<worker>().build()
-        WorkManager.getInstance(this).enqueue(request)
+
+        val mk = MasterKey.Builder(this)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+        val pref = EncryptedSharedPreferences.create(this, "ap", mk, EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+        }
+
+        if (pref.getBoolean("service", false)) {
+            icon_service.setImageResource(R.drawable.play_button)
+            activate_info.setImageResource(R.drawable.circle_green)
+        }
 
         if (db.select()) {
 
@@ -72,7 +104,6 @@ class MainActivity : AppCompatActivity() {
                     )
                     logs_list[position].time = String(c.doFinal(Base64.getDecoder().decode(time)))
                 }
-                Log.e("lista", logs_list.toString())
                 withContext(Dispatchers.Main) {
                     adapter.update(logs_list)
                 }
@@ -99,6 +130,71 @@ class MainActivity : AppCompatActivity() {
 
                 delay(50)
             }
+        }
+
+
+        service_status.setOnClickListener {
+            val dialog_activate = AlertDialog.Builder(this)
+
+            dialog_activate.setNegativeButton("Close") {_, _ ->}
+
+            if (pref.getBoolean("service", false)) {
+                dialog_activate.setTitle("You want to disable logging?")
+                dialog_activate.setMessage("If you disable logging, you will no longer be able to record logs until you enable it again.")
+                dialog_activate.setPositiveButton("Desactivate") {_, _ ->
+                    val promt = BiometricPrompt.PromptInfo.Builder()
+                        .setTitle("Authenticate yourself")
+                        .setAllowedAuthenticators(BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG)
+                        .build()
+
+                    BiometricPrompt(this, ContextCompat.getMainExecutor(this), object: BiometricPrompt.AuthenticationCallback() {
+
+                        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                            super.onAuthenticationSucceeded(result)
+                            icon_service.setImageResource(R.drawable.pause_button)
+                            activate_info.setImageResource(R.drawable.circle_red)
+                            pref.edit().putBoolean("service", false).commit()
+                            stopService(Intent(applicationContext, log_regi::class.java))
+                            val manager = getSystemService(NotificationManager::class.java)
+                            manager.cancel(1)
+                        }
+
+                        override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                            super.onAuthenticationError(errorCode, errString)
+                            Toast.makeText(applicationContext, "Authentication error", Toast.LENGTH_SHORT).show()
+                        }
+                    }).authenticate(promt)
+                }
+            }else {
+                dialog_activate.setTitle("You want to enable log logging")
+                dialog_activate.setMessage("If you enable this option, NowiLock will record your phone's logs. This may result in increased battery consumption.")
+                dialog_activate.setPositiveButton("Activate") {_, _ ->
+                    val promt = BiometricPrompt.PromptInfo.Builder()
+                        .setTitle("Authenticate yourself")
+                        .setAllowedAuthenticators(BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG)
+                        .build()
+
+                    BiometricPrompt(this, ContextCompat.getMainExecutor(this), object: BiometricPrompt.AuthenticationCallback() {
+
+                        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                            super.onAuthenticationSucceeded(result)
+                            icon_service.setImageResource(R.drawable.play_button)
+                            activate_info.setImageResource(R.drawable.circle_green)
+                            pref.edit().putBoolean("service", true).commit()
+                            ContextCompat.startForegroundService(applicationContext, Intent(applicationContext, log_regi::class.java))
+                            Toast.makeText(applicationContext, "Registration activated", Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                            super.onAuthenticationError(errorCode, errString)
+                            Toast.makeText(applicationContext, "Authentication error", Toast.LENGTH_SHORT).show()
+                        }
+                    }).authenticate(promt)
+                }
+            }
+
+
+            dialog_activate.show()
         }
 
         window.setFlags(
---
+++ app/src/main/java/com/nowilock/log_regi.kt
@@ -0,0 +1,80 @@
+package com.nowilock
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import java.security.KeyStore
+import java.time.LocalDateTime
+import java.util.Base64
+import javax.crypto.Cipher
+
+class log_regi: Service() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun noti(): Notification {
+
+        val channel = NotificationChannel("noti_lock", "channel_oti", NotificationManager.IMPORTANCE_LOW)
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+
+        return NotificationCompat.Builder(this, "noti_lock").apply {
+            setContentTitle("NowiLock")
+            setContentText("Log logging is active")
+            setSmallIcon(R.drawable.padlock)
+        }
+            .build()
+    }
+
+     @RequiresApi(Build.VERSION_CODES.O)
+     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+
+        startForeground(1, noti())
+
+        val broadcast = object: BroadcastReceiver() {
+            override fun onReceive(con: Context?, intent: Intent?) {
+                if (intent?.action == Intent.ACTION_USER_PRESENT) {
+                    val mk = MasterKey.Builder(applicationContext)
+                        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                        .build()
+                    val pref = EncryptedSharedPreferences.create(applicationContext, "ap", mk,
+                        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
+
+                    val ks = KeyStore.getInstance("AndroidKeyStore").apply { load(null) }
+
+                    val db = db(applicationContext)
+
+                    val c = Cipher.getInstance("AES/GCM/NoPadding")
+                    c.init(Cipher.ENCRYPT_MODE, ks.getKey(pref.getString("key", null), null))
+
+                    db.insert(Base64.getEncoder().withoutPadding().encodeToString(c.doFinal(
+                        LocalDateTime.now().toString().split("T").joinToString(" | ").toByteArray())), "Put a note",
+                        Base64.getEncoder().withoutPadding().encodeToString(c.iv))
+                }
+            }
+
+        }
+
+        registerReceiver(broadcast, IntentFilter(Intent.ACTION_USER_PRESENT))
+
+        return START_STICKY
+
+    }
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+
+}
